21250b513bc4d0e12f58b6c48c7f2c61af015fde
Juan Saiz <jcsaizc@gmail.com>
2021-10-17 22:00:38 -0500

    Add more tests

--- src/test/java/com/magneto/detector/services/MutantDetectorTest.java
+++ src/test/java/com/magneto/detector/services/MutantDetectorTest.java
@@ -18,4 +18,38 @@ public class MutantDetectorTest {
 
         Assertions.assertTrue(mutantDetector.isMutant(dna));
     }
+
+    @Test
+    public void testIsNotMutant() throws InvalidDnaSequence {
+        String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CACCTA","TCACTG"};
+
+        Assertions.assertFalse(mutantDetector.isMutant(dna));
+    }
+
+    @Test
+    public void testInvalidADN() {
+        String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTGE"};
+
+        Exception exception = Assertions.assertThrows(InvalidDnaSequence.class, () -> mutantDetector.isMutant(dna));
+
+        Assertions.assertEquals(InvalidDnaSequence.INVALID_DNA, exception.getMessage());
+    }
+
+    @Test
+    public void testInvalidADNSize() {
+        String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTE"};
+
+        Exception exception = Assertions.assertThrows(InvalidDnaSequence.class, () -> mutantDetector.isMutant(dna));
+
+        Assertions.assertEquals(InvalidDnaSequence.INVALID_DNA, exception.getMessage());
+    }
+
+    @Test
+    public void testInvalidEmpty() {
+        String[] dna = {};
+
+        Exception exception = Assertions.assertThrows(InvalidDnaSequence.class, () -> mutantDetector.isMutant(dna));
+
+        Assertions.assertEquals(InvalidDnaSequence.EMPTY, exception.getMessage());
+    }
 }
